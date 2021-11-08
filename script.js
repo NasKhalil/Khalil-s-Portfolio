4aa0d40722614e11391e50e000eedbc32ed5e9a4
Anuar Shaidenov <anuarshaidenov@gmail.com>
2021-11-08 20:15:39 +0600

    Style the mobile menu and add js functionality

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const btnOpen = document.querySelector(".nav-btn");
 const btnClose = document.querySelector(".close-btn");
+const menu = document.querySelector(".nav-items");
 
 btnOpen.addEventListener("click", openMenu);
 btnClose.addEventListener("click", closeMenu);
@@ -7,9 +8,11 @@ btnClose.addEventListener("click", closeMenu);
 function openMenu() {
   btnClose.style.display = "block";
   btnOpen.style.display = "none";
+  menu.style.display = "block";
 }
 
 function closeMenu() {
   btnClose.style.display = "none";
   btnOpen.style.display = "block";
+  menu.style.display = "none";
 }
