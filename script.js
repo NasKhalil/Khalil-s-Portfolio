fade4ed8a6a9f07146d49b0db9f4966cdba51616
khalilnasri92@gmail.com <Kh@lil92>
2021-11-09 14:24:39 +0100

    add projects objects to js file and style to popup window

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const btnOpen = document.querySelector('.nav-btn');
 const btnClose = document.querySelector('.close-btn');
 const menu = document.querySelector('.nav-items');
 const menuItem = document.querySelectorAll('.menu-item');
+const popupOpen = document.querySelectorAll('.project-button')
+const popupWindow = document.querySelector('.popup-window')
+const closePopupBtn = document.querySelector('.close-popup-btn')
+const popupTitle = document.querySelector('.popup-title')
 
 const data = [
   {
@@ -9,9 +13,40 @@ const data = [
     company: 'Canopi',
     role: 'Backend dev',
     year: 2015,
-    img: '',
-    description:'',
-    tags: ['javascript', 'html', 'css']
+    img: '/Media/Snapshoot-Portfolio-1.png',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    descriptionPopup:'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae dolorem aspernatur, sapiente in aut iste libero impedit suscipit dignissimos, eligendi tempora amet rem minima, aliquid nesciunt dicta hic facere distinctio! Assumenda beatae iusto voluptas sint neque, praesentium earum harum corrupti sunt delectus dolores ut a excepturi officiis consequuntur facilis, minima ducimus adipisci. Molestias, hic. Quo accusantium mollitia esse minus harum. Eos fugiat perspiciatis repudiandae molestiae? Saepe, soluta sint magnam et magni ut odio alias natus molestias est non possimus, assumenda in debitis optio similique quod ratione ad? Minus, provident asperiores? Sed quo magnam temporibus in, nam nostrum sunt asperiores modi possimus, qui doloribus harum ex sit tempora earum, eligendi consequatur fugit dicta accusamus?Vitae ipsa perferendis ad at quisquam maxime.',
+    tags: ['javascript', 'html', 'css', 'javascript', 'html', 'css']
+  },
+  {
+    title: 'Multi-Post Stories',
+    company: 'Facebook',
+    role: 'Backend dev',
+    year: 2015,
+    img: '/Media/Snapshoot-Portfolio-2.png',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    descriptionPopup:'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae dolorem aspernatur, sapiente in aut iste libero impedit suscipit dignissimos, eligendi tempora amet rem minima, aliquid nesciunt dicta hic facere distinctio! Assumenda beatae iusto voluptas sint neque, praesentium earum harum corrupti sunt delectus dolores ut a excepturi officiis consequuntur facilis, minima ducimus adipisci. Molestias, hic. Quo accusantium mollitia esse minus harum. Eos fugiat perspiciatis repudiandae molestiae? Saepe, soluta sint magnam et magni ut odio alias natus molestias est non possimus, assumenda in debitis optio similique quod ratione ad? Minus, provident asperiores? Sed quo magnam temporibus in, nam nostrum sunt asperiores modi possimus, qui doloribus harum ex sit tempora earum, eligendi consequatur fugit dicta accusamus?Vitae ipsa perferendis ad at quisquam maxime.',
+    tags: ['javascript', 'html', 'css', 'javascript', 'html', 'css']
+  },
+  {
+    title: 'Facebook 360',
+    company: 'Facebook',
+    role: 'Backend dev',
+    year: 2015,
+    img: '/Media/Snapshoot-Portfolio-3.png',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    descriptionPopup:'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae dolorem aspernatur, sapiente in aut iste libero impedit suscipit dignissimos, eligendi tempora amet rem minima, aliquid nesciunt dicta hic facere distinctio! Assumenda beatae iusto voluptas sint neque, praesentium earum harum corrupti sunt delectus dolores ut a excepturi officiis consequuntur facilis, minima ducimus adipisci. Molestias, hic. Quo accusantium mollitia esse minus harum. Eos fugiat perspiciatis repudiandae molestiae? Saepe, soluta sint magnam et magni ut odio alias natus molestias est non possimus, assumenda in debitis optio similique quod ratione ad? Minus, provident asperiores? Sed quo magnam temporibus in, nam nostrum sunt asperiores modi possimus, qui doloribus harum ex sit tempora earum, eligendi consequatur fugit dicta accusamus?Vitae ipsa perferendis ad at quisquam maxime.',
+    tags: ['javascript', 'html', 'css', 'javascript', 'html', 'css']
+  },
+  {
+    title: 'Uber Navigation',
+    company: 'Uber',
+    role: 'Backend dev',
+    year: 2018,
+    img: '/Media/Snapshoot-Portfolio-4.png',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    descriptionPopup:'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae dolorem aspernatur, sapiente in aut iste libero impedit suscipit dignissimos, eligendi tempora amet rem minima, aliquid nesciunt dicta hic facere distinctio! Assumenda beatae iusto voluptas sint neque, praesentium earum harum corrupti sunt delectus dolores ut a excepturi officiis consequuntur facilis, minima ducimus adipisci. Molestias, hic. Quo accusantium mollitia esse minus harum. Eos fugiat perspiciatis repudiandae molestiae? Saepe, soluta sint magnam et magni ut odio alias natus molestias est non possimus, assumenda in debitis optio similique quod ratione ad? Minus, provident asperiores? Sed quo magnam temporibus in, nam nostrum sunt asperiores modi possimus, qui doloribus harum ex sit tempora earum, eligendi consequatur fugit dicta accusamus?Vitae ipsa perferendis ad at quisquam maxime.',
+    tags: ['javascript', 'html', 'css', 'javascript', 'html', 'css']
   }
 ]
 
@@ -32,3 +67,17 @@ btnClose.addEventListener('click', closeMenu);
 menuItem.forEach((item) => {
   item.addEventListener('click', closeMenu);
 });
+
+function openPopup() {
+ popupWindow.style.display = 'block';
+}
+
+function closePopup() {
+  popupWindow.style.display = 'none';
+}
+
+popupOpen.forEach((item) => {
+  item.addEventListener('click', openPopup);
+})
+
+closePopupBtn.addEventListener('click', closePopup)
