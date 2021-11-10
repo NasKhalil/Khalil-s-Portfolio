c411033eef4cd6e6fe6321e3884b569c02f4e46f
khalilnasri92@gmail.com <Kh@lil92>
2021-11-10 16:10:48 +0100

    fix linters error

--- js/formValidation.js
+++ js/formValidation.js
@@ -1,30 +1,30 @@
-function main () {
+function main() {
   const emailValidationRegex = /^[a-z]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
-const email = document.getElementById('mail');
-const submitForm = document.querySelector('#contact-form');
-const errorMessage = document.querySelector('.error-message');
+  const email = document.getElementById('mail');
+  const submitForm = document.querySelector('#contact-form');
+  const errorMessage = document.querySelector('.error-message');
 
-function checkEmail(emailInput) {
-  if (emailInput.trim() === '') {
-    return false;
+  function checkEmail(emailInput) {
+    if (emailInput.trim() === '') {
+      return false;
+    }
+    if (!emailValidationRegex.test(emailInput)) {
+      return false;
+    }
+    return true;
   }
-  if (!emailValidationRegex.test(emailInput)) {
-    return false;
-  }
-  return true;
-}
 
-submitForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const emailValue = email.value;
-  if (checkEmail(emailValue)) {
-    errorMessage.style.display = 'none';
-    submitForm.submit();
-  } else {
-    errorMessage.style.display = 'block';
-    errorMessage.textContent = 'Please enter a valid email';
-  }
-});
+  submitForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const emailValue = email.value;
+    if (checkEmail(emailValue)) {
+      errorMessage.style.display = 'none';
+      submitForm.submit();
+    } else {
+      errorMessage.style.display = 'block';
+      errorMessage.textContent = 'Please enter a valid email';
+    }
+  });
 }
 
 window.onload = main;
