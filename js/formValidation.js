e8ca0d5ed5b0e74f6dd539e0beef5f46d623d3eb
Anuar Shaidenov <anuarshaidenov@gmail.com>
2021-11-10 19:42:04 +0600

    Style error message and submit form function

--- js/formValidation.js
+++ js/formValidation.js
@@ -1,20 +1,28 @@
 var emailValidationRegex = /^[a-z]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
 var email = document.getElementById('mail');
 var submitForm = document.querySelector('#contact-form');
+var errorMessage = document.querySelector('.error-message');
 
-function checkEmail() {
-    if (email.value.trim() === ""){
-        alert('please enter your email')
-      return false
-    }
-    if (!emailValidationRegex.test(email.value)){
-        alert('please use only lowercase')
-        return false
-    }
+function checkEmail(emailInput) {
+  if (emailInput.trim() === '') {
+    // alert('please enter your email');
+    return false;
+  }
+  if (!emailValidationRegex.test(emailInput)) {
+    // alert('please use only lowercase');
+    return false;
+  }
+  return true;
 }
 
-submitForm.addEventListener('submit', function(e) {
-    if(checkEmail() === false){
-        e.preventDefault();
-    }
+submitForm.addEventListener('submit', function (e) {
+  e.preventDefault();
+  const emailValue = email.value;
+  if (checkEmail(emailValue)) {
+    errorMessage.style.display = 'none';
+    submitForm.submit();
+  } else {
+    errorMessage.style.display = 'block';
+    errorMessage.textContent = 'Please enter a valid email';
+  }
 });
