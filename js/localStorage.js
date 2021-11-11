f6aa027bc1a2fe89ae28e3645e28a644d14cc850
Anuar Shaidenov <anuarshaidenov@gmail.com>
2021-11-11 20:04:49 +0600

    Add function for loading local storage data

--- js/localStorage.js
+++ js/localStorage.js
@@ -8,14 +8,26 @@ const inputData = {
   text: '',
 };
 
-function saveData() {
-    inputData.name = inputName.value;
-    inputData.email = inputEmail.value;
-    inputData.text = inputText.value;
-    inputDataStr = JSON.stringify(inputData);
-    localStorage.inputData = inputDataStr;
+function loadData() {
+  if (!localStorage.inputData) {
+    return;
   }
-  
-  inputName.addEventListener('input', saveData);
-  inputEmail.addEventListener('input', saveData);
-  inputText.addEventListener('input', saveData);
+  const loadedData = JSON.parse(localStorage.inputData);
+  inputName.value = loadedData.name;
+  inputEmail.value = loadedData.email;
+  inputText.value = loadedData.text;
+}
+
+function saveData() {
+  inputData.name = inputName.value;
+  inputData.email = inputEmail.value;
+  inputData.text = inputText.value;
+  inputDataStr = JSON.stringify(inputData);
+  localStorage.inputData = inputDataStr;
+}
+
+window.addEventListener('load', loadData);
+
+inputName.addEventListener('input', saveData);
+inputEmail.addEventListener('input', saveData);
+inputText.addEventListener('input', saveData);
